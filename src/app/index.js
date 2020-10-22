a6b3d8d3bb7eeb92b25dca7cc139b6da091ab6bf
Angel Robles <angelrobles05@gmail.com>
2020-10-22 10:01:46 -0400

    First commint with Dashboar and the taskListComponents

---
+++ src/app/index.js
@@ -0,0 +1,5 @@
+import { store } from "./store";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Main } from "../app/components/Main";
+ReactDOM.render( < Main / > , document.getElementById("app"));
